386d6c7b4a1bbf633b229aa3187b206b2ead526f
Ruan Gustavo <dev.ruangustavo@gmail.com>
2024-08-11 18:48:50 -0300

    feat: add buttons to increment and decrement in FormWorkload

--- src/components/form-workload.tsx
+++ src/components/form-workload.tsx
@@ -1,3 +1,5 @@
+import { MinusIcon, PlusIcon } from 'lucide-react'
+import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
 
@@ -49,25 +51,88 @@ export function FormWorkload({
   }
 
   return (
-    <form method="post">
+    <>
       <div className="space-y-2">
         <Label>Horas de estudo diário</Label>
-        <Input
-          type="number"
-          placeholder="4"
-          value={hoursPerDay}
-          onChange={handleHoursPerDayChange}
-        />
+        <div className="flex items-center gap-3">
+          <Input
+            type="number"
+            placeholder="4"
+            value={hoursPerDay}
+            onChange={handleHoursPerDayChange}
+            className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+          />
+          <div className="flex items-center gap-1.5">
+            <Button
+              size="sm"
+              variant="noShadow"
+              onClick={() => {
+                const MINIMUM_HOURS_PER_DAY = 1
+
+                if (hoursPerDay > MINIMUM_HOURS_PER_DAY) {
+                  onHoursPerDayChange(hoursPerDay - 1)
+                }
+              }}
+            >
+              <MinusIcon className="size-3" />
+            </Button>
+            <Button
+              size="sm"
+              variant="noShadow"
+              onClick={() => {
+                const MAXIMUM_HOURS_PER_DAY = 24
+
+                if (hoursPerDay < MAXIMUM_HOURS_PER_DAY) {
+                  onHoursPerDayChange(hoursPerDay + 1)
+                }
+              }}
+            >
+              <PlusIcon className="size-3" />
+            </Button>
+          </div>
+        </div>
       </div>
       <div className="space-y-2">
         <Label>Dias por semana</Label>
-        <Input
-          type="number"
-          placeholder="5"
-          value={daysPerWeek}
-          onChange={handleDaysPerWeekChange}
-        />
+
+        <div className="flex items-center gap-3">
+          <Input
+            type="number"
+            placeholder="5"
+            value={daysPerWeek}
+            onChange={handleDaysPerWeekChange}
+            className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+          />
+          <div className="flex items-center gap-1.5">
+            <Button
+              size="sm"
+              variant="noShadow"
+              onClick={() => {
+                const MINIMUM_DAYS_PER_WEEK = 1
+
+                if (daysPerWeek > MINIMUM_DAYS_PER_WEEK) {
+                  onDaysPerWeekChange(daysPerWeek - 1)
+                }
+              }}
+            >
+              <MinusIcon className="size-3" />
+            </Button>
+            <Button
+              size="sm"
+              variant="noShadow"
+              onClick={() => {
+                const MAXIMUM_DAYS_PER_WEEK = 7
+
+                if (daysPerWeek < MAXIMUM_DAYS_PER_WEEK) {
+                  onDaysPerWeekChange(daysPerWeek + 1)
+                }
+              }}
+            >
+              <PlusIcon className="size-3" />
+            </Button>
+          </div>
+        </div>
       </div>
-    </form>
+    </>
   )
 }
